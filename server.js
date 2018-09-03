8c21d1a342c9da09905401c10e7c4a191a67ec31
Chandra <chandramukherjee@gmail.com>
2018-09-02 23:46:05 -0400

    Just changing display message for testing CM2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('This is the new world\n');
+    response.end('This is the new world CM2\n');
 }).listen(8080);
 
 console.log('Server started');
